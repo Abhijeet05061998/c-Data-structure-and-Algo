28de57a6f3e49472e6128432544a52a4657cf597
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2020-01-03 10:17:19 +0530

    Division of array for 4

---
+++ array_devided.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+
+    int n;
+    cin>>n;
+    int arr[100];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        int dev=arr[i]/4;
+        if(arr[i]%4==0)
+        {
+            cout<<dev<<" ";
+        }
+        else
+        {
+            cout<<"-1"<<" ";
+        }
+    }
+}
