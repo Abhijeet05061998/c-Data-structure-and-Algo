39a48e484b18bec75c09b522f5e79134111b58b8
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2020-04-11 00:39:50 +0530

    inbuiltpq_function(PQ)

---
+++ inbuiltpq.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main()
+{
+    priority_queue<int>pq;
+    pq.push(16);
+    pq.push(1);
+    pq.push(167);
+    pq.push(7);
+    pq.push(65);
+    pq.push(32);
+
+
+    cout<<"Size:"<<pq.size()<<endl;
+    cout<<"Top:"<<pq.top()<<endl;
+    while(!pq.empty())
+    {
+        cout<<pq.top()<<endl;
+        pq.pop();
+    }
+}
+
